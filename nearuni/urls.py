07306ff09df280e843db98dbc9bd4bbd03d0618b
Rehoboth Ajose <ajoserehoboth049@gmail.com>
2026-02-21 12:36:45 +0100

    Add health check endpoint to URL patterns

--- nearuni/urls.py
+++ nearuni/urls.py
@@ -22,6 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('health/', health),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
