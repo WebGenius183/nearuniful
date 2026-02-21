aa1d8c9719c042379b631039ca1a7de9c3a577c1
Rehoboth Ajose <ajoserehoboth049@gmail.com>
2026-02-21 18:09:07 +0100

    Update ALLOWED_HOSTS for production environment

--- nearuni/settings.py
+++ nearuni/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://nearuniful-1.onrender.com']
 
 
 # Application definition
